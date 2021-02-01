c1b19d6ec57a0cefc6752fd83b4356ee0dc85750
deadman1994 <chahtouralii@gmail.com>
2021-02-01 23:24:50 +0100

    ajout todos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div className="chahtour">home</div>;
+  const [messagesList, setMessagesList] = useState([]);
+  const [message, setMessage] = useState("");
+  const onSubmit = () => {
+    setMessagesList((prev) => [...prev, message]);
+    setMessage("");
+  };
+  return (
+    <div>
+      <input onChange={(e) => setMessage(e.target.value)} value={message} />
+      <button onClick={onSubmit}>click</button>
+      <ul>
+        {messagesList.map((val, index) => {
+          return <li key={index}>{val}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
